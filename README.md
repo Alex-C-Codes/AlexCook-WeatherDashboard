b9ce2364e1e5e1381d41a17140b75bf2be25de06
Alex-C <alexcook1613@gmail.com>
2022-11-06 16:12:15 -0500

    added responsiveness to UI. Updated 5-day forecast to have margins between columns. Moved Current Conditions and 5-Day Forecast code from index.html to script.js. Added weather icon. Changed stylings within CSS.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AlexCook-WeatherDashboard
+## Weather Dashboard by Alex Cook
+
+This app allows people to search for a city and then find current weather conditions in that city as well as forecasted future weather conditions for that city. The dates, icons representing the day's weather conditions, the temperature, the humidity, and the wind speed are shown for each forecasted date and the current day. Moreover, once a city is searched, that city is added to the search history. People can click on the searched city within the search history to be presented again with current and future weather conditions.
+
+Here is the deployed link to the project: https://alex-c-codes.github.io/AlexCook-WeatherDashboard/
+ 
+Here is the link to the github repo for the project: https://github.com/Alex-C-Codes/AlexCook-WeatherDashboard
+
+Here is a screenshot of the project:
+![Alt text](/assets/images/app-preview.png "WeatherDashboard")
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,80 +5,145 @@ var latitude;
 var longitude;
 var searchBarEl = document.getElementById('searchBar');
 var currentConditionsContainer = document.getElementById('currentConditions');
-var weatherIcon = ' &#9728;';
+var weatherIcon;
+// var weatherIcon = ' &#9728;';
+
+// Search History variables
+var searchHistoryContainer = document.getElementById('searchHistory');
+var searchedCitiesArr = [];
 
 // Current Conditions variables
 var cityNameEl = document.getElementById('cityName');
+var currentConditionsIconEl = document.getElementById('currentConditionsIcon');
 var tempEl = document.getElementById('temp');
 var windEl = document.getElementById('wind');
 var humidityEl = document.getElementById('humidity');
+
+// 5-Day Forecast variables
+var forecastsContainer = document.getElementById('forecasts');
+var forecastDateArr = [];
 
 // Weather API calls
 var geocodingURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
 var currentConditionsURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+APIKey;
 var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid='+APIKey;
+var iconURL = 'http://openweathermap.org/img/wn/'+weatherIcon+'@2x.png'
+
 
 // gets search query from user's input into the search bar
 function getSearch() {
     city = searchBarEl.value;
-    geocodingURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
+    geocodingURL = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey+'&units=imperial';
 
     // fetches the weather API
     fetch(geocodingURL)
         .then((response) => response.json())
         .then((data) => {
-            console.log('geocodingURL');
-            console.log(data);
 
             latitude = data.coord.lat;
             longitude = data.coord.lon;
-            weatherIcon = data.weather[0].icon;
 
-            // populate Current Conditions box
-            // [ ] need to update Temp so that it's farhenheit
-            city = city[0].toUpperCase() + city.slice(1);
-            cityNameEl.innerHTML = city + getCurrentDay() + weatherIcon;
-            tempEl.innerHTML = 'Temp: ' + data.main.temp + '°F';
-            windEl.innerHTML = 'Wind: ' + data.wind.speed + ' MPH';
-            humidityEl.innerHTML = 'Humidity: ' + data.main.humidity + ' %';
+            let display = [];
 
-            getCurrentConditions();
+            display.push(
+                '<h2 id="cityName">'+data.name + getCurrentDay()+'</h2>'
+                +'<img id="currentDayImgID" src="'+getWeatherIcon(data.weather[0].icon, "currentDayImgID")+'"/>'
+                +'<ul>'
+                +    '<li id="temp">'+'Temp: '+ data.main.temp + '°F'+'</li>'
+                +    '<li id="wind">Wind: '+data.wind.speed+' MPH</li>'
+                +    '<li id="humidity">Humidity: '+data.main.humidity+' %</li>'
+                +'</ul>'
+            );
+
+            currentConditionsContainer.classList.add('border');
+            currentConditionsContainer.classList.add('border-dark');
+            currentConditionsContainer.innerHTML = display.join(' ');
+
             getForecast();
-
-        });
-}
-
-function getForecast() {
-    forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid='+APIKey;
-
-    fetch(forecastURL)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('getForecast');
-            console.log(data);
-            console.log(data.list);
-        });
-}
-
-function getCurrentConditions() {
-    currentConditionsURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid='+APIKey;
-
-    fetch(currentConditionsURL)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('getCurrentConditions');
-            console.log(data);
+            buildSearchHistorySection(data.name);
         })
+        .catch(err=>console.log(err))
 }
 
+// gets weather icon
+function getWeatherIcon(weatherIconID, imageID) {
+    iconURL = 'http://openweathermap.org/img/wn/'+weatherIconID+'@2x.png';
+    return iconURL;
+}
+
+// gets current day to display within current conditions section
 function getCurrentDay() {
     let today = new Date();
     let dd = String(today.getDate()).padStart(2,'0');
     let mm = String(today.getMonth() + 1).padStart(2,'0'); //January is 0
     let yyyy = today.getFullYear();
-    yyyy = yyyy.toString();
-    yyyy = yyyy[2] + yyyy[3];
     
     today = ' (' + mm + '/' + dd + '/' + yyyy + ') ';
     return today;
+}
+
+// builds 5-Day Forecast section
+function getForecast() {
+    forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid='+APIKey+'&units=imperial';
+
+    fetch(forecastURL)
+        .then((response) => response.json())
+        .then((data) => {
+
+            // builds dates for the 5-day forecast section
+            let yyyy;
+            let mm;
+            let dd;
+            let forecastDate;
+
+            let display = [];
+            let j = 6;
+
+            // create the display for the 5-day forecast
+            for (let i = 0; i < 5; i++) {
+                yyyy = data.list[j].dt_txt.slice(0, 4);
+                mm = data.list[j].dt_txt.slice(5, 7);
+                dd = data.list[j].dt_txt.slice(8, 10);
+                forecastDate = mm + '/' + dd + '/' + yyyy;
+
+                forecastDateArr.push(forecastDate);
+
+                display.push(
+                        '<div class="forecastBox col gx-2">'
+                        +    '<ul>'
+                        +        '<li>'+forecastDateArr[i]+'</li>'
+                        +        '<li><img id="imageID'+i+'" src="'+getWeatherIcon(data.list[j].weather[0].icon, "imageID"+i)+'"/></li>'
+                        +        '<li>Temp: '+data.list[j].main.temp+'°F</li>'
+                        +        '<li>Wind: '+data.list[j].wind.speed+' MPH</li>'
+                        +        '<li>Humidity: '+data.list[j].main.humidity+' %</li>'
+                        +    '</ul>'
+                        +'</div>'
+                );
+
+                j = j + 8;
+
+            }
+
+            forecastsContainer.innerHTML = '<h4 style="padding-top:4%;">5-Day Forecast:</h4>' + display.join(' ');
+        })
+        .catch(err=>console.log(err))
+}
+
+// builds search history section
+function buildSearchHistorySection(searchedCity) {
+    searchedCitiesArr.push(searchedCity);
+    let display = [];
+
+    for (let i = 0; i < searchedCitiesArr.length; i++) {
+        display.push(
+            '<button id="'+i+'" type="button" class="searchedCity" onclick="searchCityAgain('+i+');">'+searchedCitiesArr[i]+'</button>'
+        );
+    }
+    searchHistoryContainer.innerHTML = display.join(' ');
+}
+
+// this function is activated once someone clicks on a city in the search history
+function searchCityAgain(i) {
+    searchBarEl.value = searchedCitiesArr[i];
+    getSearch();
 }
